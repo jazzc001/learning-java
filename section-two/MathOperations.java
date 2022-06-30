ae033778ac974b5b74024913133750c2a85c38ba
jazzc001 <jazzcheung212@gmail.com>
2022-06-30 21:26:27 +0100

    learnt operators

---
+++ section-two/MathOperations.java
@@ -0,0 +1,15 @@
+public class MathOperations {
+  public static void main(String[] args) {
+      int bagOfSweets1 = 5;
+      int bagOfSweets2 = 10;
+
+
+      long starsInMilkyWay = 250000000L;
+      long starsInAndromeda = 100000000000L;
+
+      double testScore = 6.7;
+      double bonusMarks = 2.5;
+
+      System.out.println(bagOfSweets1 + testScore);
+  }
+}
