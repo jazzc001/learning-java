0cffa88f223325afd9fc898b00bb36e4989647d7
jazzc001 <jazzcheung212@gmail.com>
2022-06-30 20:29:30 +0100

    learning String

--- section-two/Bus.java
+++ section-two/Bus.java
@@ -1,7 +1,9 @@
 public class Bus {
     public static void main(String[] args) {
+        String sentence = "Number of passengers: ";
+        System.out.println(sentence);
         int passengers = 0;
-        passengers = passengers + 1;
+        passengers += 1;
         System.out.println(passengers);
     }
 }
