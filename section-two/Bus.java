54d98aa021461baf9e2a118f5572f7e4dd38c71c
jazzc001 <jazzcheung212@gmail.com>
2022-06-30 20:17:04 +0100

    updating variables

---
+++ section-two/Bus.java
@@ -0,0 +1,7 @@
+public class Bus {
+    public static void main(String[] args) {
+        int passengers = 0;
+        passengers = passengers + 1;
+        System.out.println(passengers);
+    }
+}
---
+++ section-two/Variables.java
@@ -0,0 +1,7 @@
+public class Variables {
+    public static void main(String[] args) {
+        int people = 20;
+        System.out.println(people);
+
+    }
+}
