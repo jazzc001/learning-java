a8a6e44d03946559fa2ba19aa0805478390e3e90
jazzc001 <jazzcheung212@gmail.com>
2022-06-30 20:08:08 +0100

    added section 2 for learning variables

---
+++ section-one/YourInitials.java
@@ -0,0 +1,8 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here. 
+        System.out.println(" SSS     A  ");
+        System.out.println("S   S   A A ");
+
+    }
+}
