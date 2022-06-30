3f6f68503130b3157e9e208137d28ce3531ec32b
jazzc001 <jazzcheung212@gmail.com>
2022-06-30 20:51:47 +0100

    learnt long

---
+++ section-two/Wholenumbers.java
@@ -0,0 +1,8 @@
+public class Wholenumbers {
+    public static void main(String[] args)  {
+        int numberOfFans = 5000;
+        System.out.println("Number of fans: " + numberOfFans);
+        long globalPopulation = 7000000000L;
+        System.out.println("Global Population: " + globalPopulation);
+    }
+}
