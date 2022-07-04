5789b931b0aaf9fafa81519b8b5daf602c946cbe
jazzc001 <jazzcheung212@gmail.com>
2022-07-04 17:22:54 +0100

    if else statement

--- section-three/IfElse.java
+++ section-three/IfElse.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class IfElse {
     public static void main(String[] args) {
-        
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your grade?");
+        int grade = scan.nextInt();
+
+        if (grade > 50) {
+            System.out.println("Great job, you passed!");
+        } else {
+            System.out.println("Study hard next time!");
+        }
     }
 }
