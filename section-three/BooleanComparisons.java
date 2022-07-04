81f543ba84a37c1bbd40d19eb68beafb0a914eef
jazzc001 <jazzcheung212@gmail.com>
2022-07-04 17:15:10 +0100

    Boolean comparison

--- section-three/BooleanComparisons.java
+++ section-three/BooleanComparisons.java
@@ -8,5 +8,11 @@ public class BooleanComparisons {
 
         System.out.println(biologyGrade > chemistryGrade);
         System.out.println(mathGrade > biologyGrade);
+
+        String sentence = "I love this course";
+        String sentence2 = "I love this course";
+        System.out.println(!sentence.equals(sentence2));
+        System.out.println(sentence == sentence2);
+
     }
 }
