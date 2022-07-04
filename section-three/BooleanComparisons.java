19e092f21ea3abfa583c92e3c6c83d98ad0bf48f
jazzc001 <jazzcheung212@gmail.com>
2022-07-04 16:46:14 +0100

    boolean comparison

---
+++ section-three/BooleanComparisons.java
@@ -0,0 +1,12 @@
+public class BooleanComparisons {
+    public static void main(String[] args) {
+        int chemistryGrade = 95;
+        int biologyGrade = 65;
+        double mathGrade = 70.55;
+
+
+
+        System.out.println(biologyGrade > chemistryGrade);
+        System.out.println(mathGrade > biologyGrade);
+    }
+}
