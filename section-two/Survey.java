47afe42a74325f53e4d605c8c7a87910a8096942
jazzc001 <jazzcheung212@gmail.com>
2022-07-04 16:18:53 +0100

    upgraded the survey

--- section-two/Survey.java
+++ section-two/Survey.java
@@ -7,30 +7,30 @@ public class Survey {
         int counter = 0;
 
         System.out.println("Welcom. Thank you for talking the survey");
-        System.out.println("What is your name?");
+        System.out.println("\nWhat is your name?");
         String name = scan.nextLine();
         counter++;
 
-        System.out.println("How much money do you spend on coffee?");
+        System.out.println("\nHow much money do you spend on coffee?");
         double coffeePrice = scan.nextDouble();
         counter++;
 
-        System.out.println("How much money do you spend on fast food?");
+        System.out.println("\nHow much money do you spend on fast food?");
         double foodPrice = scan.nextDouble();
         counter++;
 
-        System.out.println("How manay times a week do you buy coffee?");
+        System.out.println("\nHow manay times a week do you buy coffee?");
         int coffeeAmount = scan.nextInt();
         counter++;
 
-        System.out.println("How many times a week do you buy fast food?");
+        System.out.println("\nHow many times a week do you buy fast food?");
         int foodAmount = scan.nextInt();
         counter++;
 
         
         System.out.println("\nThank you " + name + " for answering all " + counter + " questions!");
-        System.out.println("Your fastfood expenses are $" + (foodAmount*foodPrice) +" per week.");
-        System.out.println("Your coffee expenses are $" + (coffeeAmount*coffeePrice) +" per week.");
+        System.out.println("\nYour fastfood expenses are $" + (foodAmount*foodPrice) +" per week.");
+        System.out.println("\nYour coffee expenses are $" + (coffeeAmount*coffeePrice) +" per week.");
        scan.close();
 
 
