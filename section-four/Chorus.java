b124737fe49738e4e9491d7f99269c4e3999ea44
jazzc001 <jazzcheung212@gmail.com>
2022-07-06 17:08:02 +0100

    parameters in function

---
+++ section-four/Chorus.java
@@ -0,0 +1,9 @@
+public class Chorus {
+    public static void main(String[] args) {
+        singChorus();
+    }
+
+    public static void singChorus() {
+        System.out.println("Don't blame it ont he sunshine");
+    }
+}
