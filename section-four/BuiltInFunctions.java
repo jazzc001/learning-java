f9835aaaf6dab5501af2677339d852014da95d7f
jazzc001 <jazzcheung212@gmail.com>
2022-07-11 21:51:02 +0100

    a few builtin functions

---
+++ section-four/BuiltInFunctions.java
@@ -0,0 +1,16 @@
+public class BuiltInFunctions {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        double sine = Math.sin(1.2);
+
+        System.out.println("the sin of 1.2 is: " + sine);
+
+        double power = Math.pow(2, 4);
+        System.out.println("the power of 2 is: " + power);
+
+        double random = Math.random();
+        System.out.println("the random value is: " + random);
+    }
+}
