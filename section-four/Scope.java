9843c547cf9df362a0933f2a22e89ac73c1b90a0
jazzc001 <jazzcheung212@gmail.com>
2022-07-11 21:25:52 +0100

    class scope

--- section-four/Scope.java
+++ section-four/Scope.java
@@ -1,5 +1,12 @@
 public class Scope {
     public static void main(String[] args) {
+       
+        someFunction();
         
+    }
+
+    public static void someFunction() {
+        int apples = 5;
+        System.out.println(apples);
     }
 }
