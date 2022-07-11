7cd8419af39e44cf4240ce684992b2452f2ffe99
jazzc001 <jazzcheung212@gmail.com>
2022-07-11 21:34:38 +0100

    learnt scope of variables

--- section-four/Scope.java
+++ section-four/Scope.java
@@ -1,12 +1,15 @@
 public class Scope {
+
+    static int dogs = 5;
+
     public static void main(String[] args) {
-       
+       System.out.println(dogs);
         someFunction();
         
     }
 
     public static void someFunction() {
-        int apples = 5;
-        System.out.println(apples);
+        
+        System.out.println(dogs);
     }
 }
