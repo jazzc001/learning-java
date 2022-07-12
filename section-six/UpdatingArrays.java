29fe511871abf62d701acda884da7130f2e3c5bd
jazzc001 <jazzcheung212@gmail.com>
2022-07-12 11:51:19 +0100

    updating Arrays

--- section-six/UpdatingArrays.java
+++ section-six/UpdatingArrays.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
+
 public class UpdatingArrays {
 
     public static void main(String[] args) {
-        
+        String[] menu = {"Espresso", "Ice Coffee", "Macchiato"};
+        String menuString = Arrays.toString(menu);
+        System.out.println(menuString);
+
+        String[] newMenu = new String[5];
+
+        for (int i = 0; i < menu.length; i++) {
+            newMenu[i] = menu[i];
+        }
+
+        newMenu[3] = "House Blend";
+        newMenu[4] = "Dirty";
+        System.out.println(Arrays.toString(newMenu));
+
     }
 }
