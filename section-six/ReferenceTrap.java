b9ecf52b0c5caaea188eff76c22145f9ce3785e3
jazzc001 <jazzcheung212@gmail.com>
2022-07-14 00:40:14 +0100

    reference trap solution 2

--- section-six/ReferenceTrap.java
+++ section-six/ReferenceTrap.java
@@ -13,14 +13,24 @@ public class ReferenceTrap {
     // }
 
     // Soultion one: make a new array
+    // public static void main(String[] args) {
+    //     String[] staffLastYear = {"Tommy", "Joel", "Ellie"};
+    //     String[] staffThisYear = new String[staffLastYear.length];
+    //     for (int i = 0; i < staffThisYear.length; i++) {
+    //         staffThisYear[i] = staffLastYear[i];
+    //     }
+    //     staffThisYear[1] = "Ben";
+    //     System.out.println(Arrays.toString(staffLastYear));
+    //     System.out.println(Arrays.toString(staffThisYear));
+    // }
+
+    // Soultion two: use copy
     public static void main(String[] args) {
         String[] staffLastYear = {"Tommy", "Joel", "Ellie"};
-        String[] staffThisYear = new String[staffLastYear.length];
-        for (int i = 0; i < staffThisYear.length; i++) {
-            staffThisYear[i] = staffLastYear[i];
-        }
+        String[] staffThisYear = Arrays.copyOf(staffLastYear, staffLastYear.length);
         staffThisYear[1] = "Ben";
         System.out.println(Arrays.toString(staffLastYear));
         System.out.println(Arrays.toString(staffThisYear));
+
     }
 }
