591e1372d25490825c0d5d2698ea33547cec212a
jazzc001 <jazzcheung212@gmail.com>
2022-07-14 00:36:47 +0100

    reference trap solution one made

--- section-six/ReferenceTrap.java
+++ section-six/ReferenceTrap.java
@@ -12,10 +12,13 @@ public class ReferenceTrap {
     //     System.out.println(Arrays.toString(staffThisYear));
     // }
 
-    // Soultion one
+    // Soultion one: make a new array
     public static void main(String[] args) {
         String[] staffLastYear = {"Tommy", "Joel", "Ellie"};
-        String[] staffThisYear = staffLastYear;
+        String[] staffThisYear = new String[staffLastYear.length];
+        for (int i = 0; i < staffThisYear.length; i++) {
+            staffThisYear[i] = staffLastYear[i];
+        }
         staffThisYear[1] = "Ben";
         System.out.println(Arrays.toString(staffLastYear));
         System.out.println(Arrays.toString(staffThisYear));
