2b477af1bdb367c271a25ce746e1d8139df87e5f
jazzc001 <jazzcheung212@gmail.com>
2022-07-14 17:22:58 +0100

    how to instantiate a class

---
+++ section-seven/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Car nissan = new Car();
+        nissan.make = "nissan";
+        nissan.price = 2000;
+        nissan.year = 2000;
+        nissan.color = "blue";
+        System.out.println(nissan.make);
+        System.out.println(nissan.price);
+        System.out.println(nissan.year);
+        System.out.println(nissan.color);
+    }
+}
