fa4e04f627694500fed4b91b3cce1f78480989f2
jazzc001 <jazzcheung212@gmail.com>
2022-07-19 00:33:27 +0100

    struggling unit testing package

---
+++ UNIT-TESTING/src/main/Main.java
@@ -0,0 +1,13 @@
+package src.main;
+
+public class Main {
+    static double[] price = new double[] {2.33, 1.32, 4.32, 11.33};
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double getSubtotal() {
+        return 0;
+    }
+}
