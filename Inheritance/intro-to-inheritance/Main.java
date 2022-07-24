d97c217b5d99d42ddfa288e1aa694c074176e40f
jazzc001 <jazzcheung212@gmail.com>
2022-07-24 23:17:27 +0100

    Inheritance of Object class

--- Inheritance/intro-to-inheritance/Main.java
+++ Inheritance/intro-to-inheritance/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Person person = new Person("Jasmine", 30);
-        
+        System.out.println(person.toString());
     } 
 }
